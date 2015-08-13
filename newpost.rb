1bec88e84a5faa3440d515db70091b15c315aab8
Josh <linkingparents@gmail.com>
2015-08-13 10:36:22 -0300

    Added tags and categories. Both are output using YAML sequences.

--- newpost.rb
+++ newpost.rb
@@ -8,6 +8,8 @@ OptionParser.new do |opts|
 
   opts.on('-d', '--date YYYY-MM-DD', 'Post date in YYYY-MM-DD') { |v| options[:post_date] = v }
   opts.on('-t', '--title "Title"', 'Post Title') { |v| options[:post_title] = v }
+  opts.on('-a', '--tags "tag1 tag2"', 'Tags for post - Space delimited') { |v| options[:post_tags] = v.split }	
+  opts.on('-c', '--categories "cat1 cat2"', 'Categories for post - Space delimited') { |v| options[:post_categories] = v.split }
 
 end.parse!
 
@@ -20,6 +22,8 @@ frontmatter = {
   'layout' => 'post',
   'title' => options[:post_title],
   'date' => options[:post_date],
+  'tags' => options[:post_tags],
+  'categories' => options[:post_categories]
 }
 
 # ---
