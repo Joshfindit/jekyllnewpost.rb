bf9b9fd8b5e8f151ca419e5e3f69771bfae5988d
Josh <linkingparents@gmail.com>
2015-08-20 14:31:24 -0300

    If date is undefined, use today YYYY-MM-DD

--- newpost.rb
+++ newpost.rb
@@ -7,7 +7,7 @@ OptionParser.new do |opts|
   opts.banner = "Usage: newpost.rb [options]"
 
   opts.on('-d', '--date YYYY-MM-DD', 'Post date in YYYY-MM-DD. Can also handle YYYY and YYYY-MM') { |v| options[:post_date] = v }
-  opts.on('-t', '--title "Title"', 'Post Title') { |v| options[:post_title] = v }
+  opts.on('-t', '--title "Title"', 'Post Title. Note: Due to a bug, there must be a space in the title.') { |v| options[:post_title] = v }
   opts.on('-a', '--tags "tag1 tag2"', 'Tags for post - Space delimited') { |v| options[:post_tags] = v.split }	
   opts.on('-c', '--categories "cat1 cat2"', 'Categories for post - Space delimited') { |v| options[:post_categories] = v.split }
   opts.on('-p', '--postcontent "text"', 'Content for post') { |v| options[:post_contents] = v }
@@ -20,22 +20,25 @@ end.parse!
 # For debugging, uncomment the next line
 # puts options
 
+if options[:post_date].nil?
+  options[:post_date] = Time.now.strftime("%Y-%m-%d")
+else
+  if /^[[:digit:]][[:digit:]][[:digit:]][[:digit:]]$/.match(options[:post_date])
+    puts "Date matches only year. Adding 06-01 as a sane default"
+    options[:post_date] = options[:post_date] + "-06-01"
+    estimatedDateTag = "DateEstimated_MonthDay"
+    #Don't forget the manually added tag: DateEstimated_YearMonthDay for when the date is completely fuzzy
 
-if /^[[:digit:]][[:digit:]][[:digit:]][[:digit:]]$/.match(options[:post_date])
-  puts "Date matches only year. Adding 06-01 as a sane default"
-  options[:post_date] = options[:post_date] + "-06-01"
-  estimatedDateTag = "DateEstimated_MonthDay"
-  #Don't forget the manually added tag: DateEstimated_YearMonthDay for when the date is completely fuzzy
-
-elsif /^[[:digit:]][[:digit:]][[:digit:]][[:digit:]]\-[[:digit:]][[:digit:]]$/.match(options[:post_date])
-  puts "Date matches only year and month. Adding day 01"
-  options[:post_date] = options[:post_date] + "-01"
-  estimatedDateTag = "DateEstimated_Day"
-elsif /^[[:digit:]][[:digit:]][[:digit:]][[:digit:]]\-[[:digit:]][[:digit:]]\-[[:digit:]][[:digit:]]$/.match(options[:post_date])
-  # puts "Matches year, month, and day"
-  estimatedDateTag = nil
-else 
-  puts Date invalid!
+  elsif /^[[:digit:]][[:digit:]][[:digit:]][[:digit:]]\-[[:digit:]][[:digit:]]$/.match(options[:post_date])
+    puts "Date matches only year and month. Adding day 01"
+    options[:post_date] = options[:post_date] + "-01"
+    estimatedDateTag = "DateEstimated_Day"
+  elsif /^[[:digit:]][[:digit:]][[:digit:]][[:digit:]]\-[[:digit:]][[:digit:]]\-[[:digit:]][[:digit:]]$/.match(options[:post_date])
+    # puts "Matches year, month, and day"
+    estimatedDateTag = nil
+  else 
+    puts Date invalid!
+  end
 end
 
 
@@ -58,7 +61,7 @@ frontmatter = {
 # sequence:   3
 # ---
 
-
+#todo: If _posts doesn't exist, just write here (cannot think of a use-case where _posts should be created by this tool)
 if Dir.pwd.end_with? "_posts" or options[:file_writehere]
   isInPostsDir = true
 else
